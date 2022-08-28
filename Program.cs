5644c596e857046d33e25959507986705a0f7089
AndreyAnisimovTerricon <atriummebel79@yandex.ru>
2022-08-28 13:21:21 +0300

    Программа таблицы кубов кубов чисел от 1 до N

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число N");
+double n = double.Parse(Console.ReadLine());
+n = Math.Abs(n);
+
+Console.Write(n + "->");
+
+for(int i=1; i<=n;i++)
+{
+    Console.Write(Math.Pow(i,3)+" ");
+}
